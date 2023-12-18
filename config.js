0510f3f89a86598770f14bc3d21d2a289a72ca58
mohamedaskarasibm <mohamedaskarasibm@gmail.com>
2023-12-18 18:48:37 +0530

    update

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT;
+
+export const mongoDBURL = process.env.MONGO_URI
+  // 'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
